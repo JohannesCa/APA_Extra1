496563ddbf96223de9e6fb69deefeef88d4c7442
JohannesCa <johannes.cavalcante@outlook.com>
2016-09-29 11:18:04 -0300

    Updated binomial function.

--- BinomialCoeficient.hpp
+++ BinomialCoeficient.hpp
@@ -9,18 +9,29 @@
 #define BINOMIALCOEFICIENT_HPP_
 
 #include <iostream>
+#include <stdio.h>
 
-int BinomialCoeficient(int n, int p)
+int BinomialCoeficient(int n, int p, bool print = false)
 {
     int C[n+1][p+1]; // Declarando o array bidimensional (tabela) a ser utilizado.
 
     for(int i = 0; i <= n; ++i){  // Calcula o valor do coeficiente de baixo para cima.
-        for (int j = 0; j <= (i < p? i:p); j++){
+        for(int j = 0; j <= (i < p? i : p); j++){
             if (j == 0 || j == i)   // Casos particulares.
                 C[i][j] = 1;
             else    // Calcula o valor utilizando os que foram computados anteriormente.
                 C[i][j] = C[i-1][j-1] + C[i-1][j];
         }
+    }
+
+    if(print){
+    	printf(">> Calculando o Coeficiente C(%i,%i)\n>> Tabela:\n", n, p);
+    	for(int i = 0; i <= n; ++i){
+    		for(int j = 0; j <= p; ++j){
+    			printf("%i\t\t", C[i][j]);
+    		}
+    		std::cout << std::endl;
+    	}
     }
 
     return C[n][p];
--- main.cpp
+++ main.cpp
@@ -12,5 +12,35 @@ using namespace std;
 
 int main(void)
 {
-	return 0;
+    int n = 5, p = 2;
+    //printf ("Value of C(%d, %d) is %d ", n, p, binomialCoeff(n, p) );
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p, true) << endl;
+/*    n = 4;
+    p = 2;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p, true) << endl;
+    n = 0;
+    p = 0;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    n = 5;
+    p = 0;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    n = 0;
+    p = 9;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    n = 7;
+    p = 5;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    n = 6;
+    p = 3;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    n = 11;
+    p = 3;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    n = 5;
+    p = 13;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    n = 16;
+    p = 10;
+    cout << "O valor do coeficiente binomial C(" << n << ", " << p << ")= " << BinomialCoeficient(n, p) << endl;
+    */return 0;
 }
